443116488eadbd562eff49afeb3fee58c24fbf41
Gajendra L <100460060+Gajaindra@users.noreply.github.com>
2025-06-15 13:51:57 +0530

    app.py

--- app.py
+++ app.py
@@ -23,4 +23,4 @@ def index():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Use PORT provided by Render
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=True)
