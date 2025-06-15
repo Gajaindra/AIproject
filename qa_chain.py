9b403c148e81ddc6c7a79d789936be3877c30737
Gajendra L <100460060+Gajaindra@users.noreply.github.com>
2025-06-15 13:32:23 +0530

    qa_chain.py

--- qa_chain.py
+++ qa_chain.py
@@ -1,12 +1,12 @@
+import os
 import fitz  # PyMuPDF
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain.chains import RetrievalQA
 from langchain_groq import ChatGroq
-import os
 
-# Use environment variable for Groq API key
+# Read Groq API key from environment
 groq_api_key = os.getenv("GROQ_API_KEY")
 if not groq_api_key:
     raise ValueError("GROQ_API_KEY environment variable not set.")
