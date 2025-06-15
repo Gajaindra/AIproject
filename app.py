1b26f0d00630a725d5a3ac4972ea5ff33b57344d
Gajendra L <100460060+Gajaindra@users.noreply.github.com>
2025-06-15 14:25:36 +0530

    app.py

--- app.py
+++ app.py
@@ -22,5 +22,4 @@ def index():
     return render_template("index.html", answer=answer)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Use PORT provided by Render
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(port=10000)  # Render binds to 0.0.0.0:10000
